8b21a2e749fd14c25f11a7173b4af53739bcd461
soichiro-nitta <soic1226@gmail.com>
2021-05-25 23:55:14 +0900

    stylelint-config-prettier

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 // var ERROR = 'error'
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier',
+  ],
   ignoreFiles: ['/node_modules', '/.next', '*.*', '!*.css'],
   rules: {
     'at-rule-no-unknown': [
