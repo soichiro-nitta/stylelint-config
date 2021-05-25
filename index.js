0a9d21253d9daa188c97204568f96ad341b39334
soichiro-nitta <soic1226@gmail.com>
2021-05-25 23:49:04 +0900

    remove prettier

--- index.js
+++ index.js
@@ -3,14 +3,8 @@
 // var ERROR = 'error'
 
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-rational-order',
-    'stylelint-prettier/recommended',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
   ignoreFiles: ['/node_modules', '/.next', '*.*', '!*.css'],
-  plugins: ['stylelint-prettier'],
   rules: {
     'at-rule-no-unknown': [
       true,
